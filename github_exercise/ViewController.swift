13a2b5adc16a6919839c8ce50c14d23c7b017c37
Abinaya Sindhu <abinaya@parithi.com>
2017-08-04 12:24:38 +0530

    First commit

--- github_exercise/ViewController.swift
+++ github_exercise/ViewController.swift
@@ -12,6 +12,7 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        print ("This is a GitHub exercise")
         // Do any additional setup after loading the view, typically from a nib.
     }
 
